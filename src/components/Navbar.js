78d8aed98ba43458e46a5591c23b45b3c510a2f1
saif khan <saifbusiness29@gmail.com>
2021-12-25 17:50:05 +0530

    made the modification

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,6 +13,7 @@ const Navbar = () => {
   
 
   const [isOpen, setIsOpen] = useState(true);
+  console.log(isOpen)
 
  
 
@@ -23,22 +24,22 @@ const Navbar = () => {
         <ul className={isOpen ? "" : "show"}>
           
           
-          <li><a href="#home">Home</a></li>
-          <li> <a href="#about_22">About</a></li>
-          <li><a href="#projects_nav">Projects</a></li>
-          <li><a href="#skills_nav">Skills</a></li>
-          <li><a href="#services_nav">Services</a></li>
-          <li><a href="#youtube_nav">Youtube</a></li>
-          <li><a href="#contact_nav">Contact</a></li>
+          <li onClick={() => setIsOpen(!isOpen)}><a href="#home">Home</a></li>
+          <li onClick={() => setIsOpen(!isOpen)}><a href="#about_22">About</a></li>
+          <li onClick={() => setIsOpen(!isOpen)}><a href="#projects_nav">Projects</a></li>
+          <li onClick={() => setIsOpen(!isOpen)}><a href="#skills_nav">Skills</a></li>
+          <li onClick={() => setIsOpen(!isOpen)}><a href="#services_nav">Services</a></li>
+          <li onClick={() => setIsOpen(!isOpen)}><a href="#youtube_nav">Youtube</a></li>
+          <li onClick={() => setIsOpen(!isOpen)}><a href="#contact_nav">Contact</a></li>
         </ul>
         <img
           className={isOpen ? "navImg" : "navImg hideHam"}
           onClick={() => setIsOpen(!isOpen)}
-          src={icon}
+          src="https://user-images.githubusercontent.com/79800224/147384666-7e42472d-5562-4442-9c0b-ea93aefb2980.png"
         />
         <img
           className={isOpen ? "navImg2 " : "navImg navImg22"}
-          src={cancel2}
+          src="https://user-images.githubusercontent.com/79800224/147384667-91c735eb-31c3-4ae1-8896-616eb2c4cb7c.png"
           onClick={() => setIsOpen(!isOpen)}
         />
       </nav>
